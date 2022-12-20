3fc59239fd56e55f9849ed7e1a8491bb6009f18e
Nikhil Kumar <nkhlneil@gmail.com>
2022-12-20 15:15:57 +0100

    Added more tests

--- shipments_test.go
+++ shipments_test.go
@@ -25,6 +25,78 @@ func Test_ShipmentsSuccessEmptySlice(t *testing.T) {
   )
 }
 
+func Test_ShipmentsSuccessInitialShipments(t *testing.T) {
+  shipement1 := ShipmentInfo{Sender_country: "SE", Receiver_country: "SE", Weight: 1, Price: "100 SEK"}
+  shipement2 := ShipmentInfo{Sender_country: "SE", Receiver_country: "DK", Weight: 1, Price: "150 SEK"}
+  expected, _ := json.Marshal(ResponseGetShipments{Shipments: []ShipmentInfo{shipement1, shipement2}})
+  Helper_TestApi(
+    t,
+    http.MethodGet,
+    SHIPMENTS_API_ENDPOINT,
+    "",
+    nil,
+    string(expected),
+    http.StatusOK,
+    []ShipmentInfo{shipement1, shipement2},
+  )
+}
+
+func Test_Shipments405MethodNotAllowed(t *testing.T) {
+  expected, _ := json.Marshal(HTTPError{Detail: "Method not allowed - Expecting GET /shipments"})
+  Helper_TestApi(
+    t,
+    http.MethodPost,
+    SHIPMENTS_API_ENDPOINT,
+    "",
+    nil,
+    string(expected),
+    http.StatusMethodNotAllowed,
+    []ShipmentInfo{},
+  )
+}
+
+func Test_Shipment405MethodNotAllowed(t *testing.T) {
+  expected, _ := json.Marshal(HTTPError{Detail: "Method not allowed - Expecting POST /shipment"})
+  Helper_TestApi(
+    t,
+    http.MethodGet,
+    SHIPMENT_API_ENDPOINT,
+    "",
+    nil,
+    string(expected),
+    http.StatusMethodNotAllowed,
+    []ShipmentInfo{},
+  )
+}
+
+func Test_Shipment400NoBody(t *testing.T) {
+  expected, _ := json.Marshal(HTTPError{Detail: "Bad Request : No body present"})
+  Helper_TestApi(
+    t,
+    http.MethodPost,
+    SHIPMENT_API_ENDPOINT,
+    APPLICATION_JSON,
+    nil,
+    string(expected),
+    http.StatusBadRequest,
+    []ShipmentInfo{},
+  )
+}
+
+func Test_Shipment400UnexpectedContentType(t *testing.T) {
+  expected, _ := json.Marshal(HTTPError{Detail: "Bad Request : Content-type header is not application/json"})
+  Helper_TestApi(
+    t,
+    http.MethodPost,
+    SHIPMENT_API_ENDPOINT,
+    "text/plain",
+    nil,
+    string(expected),
+    http.StatusBadRequest,
+    []ShipmentInfo{},
+  )
+}
+
 func Test_ShipmentSuccessDomesticWithinEU(t *testing.T) {
   requestString, _ := json.Marshal(ShipmentInfo{
     Sender_country:   "SE",
@@ -36,6 +108,54 @@ func Test_ShipmentSuccessDomesticWithinEU(t *testing.T) {
     Receiver_country: "SE",
     Weight:           2.5,
     Price:            "100.00 " + SWEDISH_KRONA,
+  })
+  Helper_TestApi(
+    t,
+    http.MethodPost,
+    SHIPMENT_API_ENDPOINT,
+    APPLICATION_JSON,
+    requestString,
+    string(expected),
+    http.StatusOK,
+    []ShipmentInfo{},
+  )
+}
+
+func Test_ShipmentSuccessWithinEU(t *testing.T) {
+  requestString, _ := json.Marshal(ShipmentInfo{
+    Sender_country:   "SE",
+    Receiver_country: "DK",
+    Weight:           2.5,
+  })
+  expected, _ := json.Marshal(ShipmentInfo{
+    Sender_country:   "SE",
+    Receiver_country: "DK",
+    Weight:           2.5,
+    Price:            "150.00 " + SWEDISH_KRONA,
+  })
+  Helper_TestApi(
+    t,
+    http.MethodPost,
+    SHIPMENT_API_ENDPOINT,
+    APPLICATION_JSON,
+    requestString,
+    string(expected),
+    http.StatusOK,
+    []ShipmentInfo{},
+  )
+}
+
+func Test_ShipmentSuccessInternational(t *testing.T) {
+  requestString, _ := json.Marshal(ShipmentInfo{
+    Sender_country:   "SE",
+    Receiver_country: "IN",
+    Weight:           2.5,
+  })
+  expected, _ := json.Marshal(ShipmentInfo{
+    Sender_country:   "SE",
+    Receiver_country: "IN",
+    Weight:           2.5,
+    Price:            "250.00 " + SWEDISH_KRONA,
   })
   Helper_TestApi(
     t,
